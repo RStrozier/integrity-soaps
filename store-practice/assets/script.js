fefe1518cff220213f92029230007ce43da51d87
Rashida Strozier <105885953+RStrozier@users.noreply.github.com>
2023-08-21 11:36:06 -0400

    Add files via upload

---
+++ store-practice/assets/script.js
@@ -0,0 +1,11 @@
+var learnProductOneBtn = document.querySelector("#about-product-1-btn");
+var learnProductTwoBtn = document.querySelector("#about-product-2-btn");
+
+learnProductOneBtn.addEventListener("click", function () {
+    alert("clicked");
+});
+
+learnProductTwoBtn.addEventListener("click", function () {
+    alert("clicked");
+});
+
